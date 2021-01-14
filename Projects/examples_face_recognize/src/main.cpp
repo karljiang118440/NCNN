6337da5e7fa69abd4f680e6dac67d34ced679efb
karljiang118440 <32283913+karljiang118440@users.noreply.github.com>
2021-01-14 17:48:02 +0800

    add landmark cam

--- Projects/examples_face_recognize/src/main.cpp
+++ Projects/examples_face_recognize/src/main.cpp
@@ -2,21 +2,6 @@
 #include "face_engine.h"
 
 
-/*
-1. add threads 
-
-*/
-
-
-#define NUM_THREADS 4
-
-
-
-
-
-
-
-
 int TestLandmark(int argc, char* argv[]) {
 	cv::Mat img_src = cv::imread("../images/4.jpg");
 	const char* root_path = "../models";
@@ -48,6 +33,83 @@ int TestLandmark(int argc, char* argv[]) {
 
 }
 
+
+
+
+
+int TestLandmark_cam(int argc, char* argv[]) {
+
+
+	// cv::Mat img_src = cv::imread("../images/4.jpg");
+	const char* root_path = "../models";
+
+
+
+    cv::Mat frame;
+    cv::VideoCapture cap(0);
+
+
+
+    while (true)
+    {
+        cap >> frame ;
+
+	FaceEngine face_engine;
+	face_engine.LoadModel(root_path);
+	std::vector<FaceInfo> faces;
+
+	double start = static_cast<double>(cv::getTickCount());
+	
+	face_engine.Detect(frame, &faces);
+	for (int i = 0; i < static_cast<int>(faces.size()); ++i) {
+		cv::Rect face = faces.at(i).face_;
+		cv::rectangle(frame, face, cv::Scalar(0, 255, 0), 2);
+		std::vector<cv::Point2f> keypoints;
+
+        // double start = static_cast<double>(cv::getTickCount());
+
+		face_engine.ExtractKeypoints(frame, face, &keypoints);
+		for (int j = 0; j < static_cast<int>(keypoints.size()); ++j) {
+			cv::circle(frame, keypoints[j], 1, cv::Scalar(0, 0, 255), 1);
+		}
+
+
+	// double end = static_cast<double>(cv::getTickCount());
+	// double time_cost = (end - start) / cv::getTickFrequency() * 1000;
+	// std::cout << "time cost: " << time_cost << "ms" << std::endl;
+
+
+	}
+
+	double end = static_cast<double>(cv::getTickCount());
+	double time_cost = (end - start) / cv::getTickFrequency() * 1000;
+	std::cout << "time cost: " << time_cost << "ms" << std::endl;
+
+    cv::imshow("demo", frame);
+    cv::waitKey(1);
+
+
+    }
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
+
+
+
+
 int TestRecognize(int argc, char* argv[]) {
 	cv::Mat img_src = cv::imread("../images/4.jpg");
 	const char* root_path = "../models";
@@ -59,16 +121,31 @@ int TestRecognize(int argc, char* argv[]) {
 	face_engine.Detect(img_src, &faces);
 
 
-	cv::Mat face1 = img_src(faces[0].face_).clone();
-	cv::Mat face2 = img_src(faces[1].face_).clone();
+	// cv::Mat face1 = img_src(faces[0].face_).clone();
+	// cv::Mat face2 = img_src(faces[1].face_).clone();
+
+
+	cv::Mat face1 = cv::imread("../images/karl.jpg");
+	//cv::Mat face2 = cv::imread("../images/stark.png");
+	cv::Mat face2 = cv::imread("../images/karl2.jpg");
+
+
 	std::vector<float> feature1, feature2;
+
+	double start2 = static_cast<double>(cv::getTickCount());	
 	face_engine.ExtractFeature(face1, &feature1);
 	face_engine.ExtractFeature(face2, &feature2);
 	float sim = CalculSimilarity(feature1, feature2);
+	double end2 = static_cast<double>(cv::getTickCount());
+
+
 
 	double end = static_cast<double>(cv::getTickCount());
 	double time_cost = (end - start) / cv::getTickFrequency() * 1000;
+	double time_cost2 = (end2 - start2) / cv::getTickFrequency() * 1000;
 	std::cout << "time cost: " << time_cost << "ms" << std::endl;
+
+	std::cout << "time2 cost: " << time_cost2 << "ms" << std::endl;
 
 	for (int i = 0; i < static_cast<int>(faces.size()); ++i) {
 		cv::Rect face = faces.at(i).face_;
@@ -76,88 +153,13 @@ int TestRecognize(int argc, char* argv[]) {
 	}
 	cv::imwrite("../images/face1.jpg", face1);
 	cv::imwrite("../images/face2.jpg", face2);
-	cv::imwrite("result.jpg", img_src);
+	cv::imwrite("result1.jpg", img_src);
 	std::cout << "similarity is: " << sim << std::endl;
-
-	return 0;
-
-}
-
-
-
-int TestRecognize_pic(int argc, char* argv[]) {
-	cv::Mat img_src = cv::imread("../images/karl.jpg");
-	const char* root_path = "../models";
-
-	// double start = static_cast<double>(cv::getTickCount());
-	FaceEngine face_engine;
-	face_engine.LoadModel(root_path);
-	std::vector<FaceInfo> faces;
-	face_engine.Detect(img_src, &faces);
-
-
-	// cv::Mat face1 = img_src(faces[0].face_).clone();
-	// cv::Mat face2 = img_src(faces[1].face_).clone();
-
-	cv::Mat face1 = cv::imread("../images/karl.jpg");
-	cv::Mat face2 = cv::imread("../images/karl2.jpg");
-
-
-
-	std::vector<float> feature1, feature2;
-	face_engine.ExtractFeature(face1, &feature1);
-
-	while(1){
-
-	double start = static_cast<double>(cv::getTickCount());
-
-	face_engine.ExtractFeature(face2, &feature2);
-	float sim = CalculSimilarity(feature1, feature2);
-
-	double end = static_cast<double>(cv::getTickCount());
-	double time_cost = (end - start) / cv::getTickFrequency() * 1000;
-	std::cout << "time cost: " << time_cost << "ms" << std::endl;
-
-	for (int i = 0; i < static_cast<int>(faces.size()); ++i) {
-		cv::Rect face = faces.at(i).face_;
-		cv::rectangle(img_src, face, cv::Scalar(0, 255, 0), 2);		
-	}
-
-	std::cout << "similarity is: " << sim << std::endl;
-
-
-	}
-
-
-    // cv::VideoCapture cap;
-	// cv::Mat frame;
-    // cap.open("/home/root/airunner/data/airunner/video_clips/university_traffic.avi");
-
-    // while (true)
-    // {
-    //     cap >> frame;
-    //     double start = ncnn::get_current_time();
-
-	// 	face_engine.ExtractFeature(face2, &feature2);
-	// 	float sim = CalculSimilarity(feature1, feature2);
-
-    //     double end = ncnn::get_current_time();
-    //     double time = end - start;
-    //     printf("Time:%7.2f \n",time);
-
-    // }
-
-
 
 
 	return 0;
 
 }
-
-
-
-
-
 
 int TestAlignFace(int argc, char* argv[]) {
 	cv::Mat img_src = cv::imread("../images/4.jpg");
@@ -186,9 +188,6 @@ int TestAlignFace(int argc, char* argv[]) {
 	cv::waitKey(0);
 
 }
-
-
-
 
 int TestCenterface(int argc, char* argv[]) {
 	cv::Mat img_src = cv::imread("../images/4.jpg");
@@ -219,7 +218,9 @@ int TestCenterface(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]) {
 	// return TestLandmark(argc, argv);
-	return TestRecognize_pic(argc, argv);
+	// return TestRecognize(argc, argv);
+	return TestLandmark_cam(argc, argv);
+	
 	// return TestAlignFace(argc, argv);
-	//return TestCenterface(argc, argv);
+	// return TestCenterface(argc, argv);
 }
